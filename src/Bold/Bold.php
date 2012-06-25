29046f2cb0d620159950029da8cdb9f7d6f1b138
Henrik Kjelsberg <hkjels@me.com>
2012-06-25 11:04:23 +0200

    Bootstrapping & OO-only

--- src/Bold/Bold.php
+++ src/Bold/Bold.php
@@ -8,39 +8,6 @@ namespace Bold;
 use Bold\Router;
 use Bold\Request;
 use Bold\Response;
-use Bold\util\Console;
-use Bold\util\Hooks;
-
-/**
- * Bootstrap
- *
- * Initializes Bold and global variables
- */
-
-$GLOBALS['console'] = new Console();
-$GLOBALS['hooks'] = new Hooks();
-
-// Initiliaze helper-functions
-
-define('BOLD_ROOT', realpath(dirname(__FILE__)));
-$helpers = array_slice(scandir(BOLD_ROOT.'/helpers'), 2);
-foreach ($helpers as $helper) include BOLD_ROOT."/helpers/$helper";
-
-/**
- * A few native hooks
- */
-
-$hooks->add('middleware', function ($req, $res) {
-  $res->setHeader('X-Powered-By', 'Codeable');
-});
-$hooks->add('pre-dispatch', function () {
-  global $console;
-  $console->log('pre-dispatch');
-});
-$hooks->add('post-dispatch', function () {
-  global $console;
-  $console->output();
-});
 
 /**
  * Bold
---
+++ src/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Bootstrap
+ *
+ * Initializes Bold and global variables
+ */
+
+$GLOBALS['console'] = new Bold\Util\Console();
+$GLOBALS['hooks'] = new Bold\Util\Hooks();
+
+/**
+ * A few native hooks
+ */
+
+$hooks->add('middleware', function ($req, $res) {
+  $res->setHeader('X-Powered-By', 'Codeable');
+});
+$hooks->add('pre-dispatch', function () {
+  global $console;
+  $console->log('pre-dispatch');
+});
+$hooks->add('post-dispatch', function () {
+  global $console;
+  $console->output();
+});
+
